5f65c852a7e6a47d07fdb716215f9231fab99a74
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2023-04-08 18:18:30 +0200

    btn more left

--- my-project/src/components/Header.js
+++ my-project/src/components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Header (){
     return(
-      <div className='fixed sm:top-6 top-4 right-16 sm:right-6 z-10 text-[8px] sm:text-sm flex flex-col sm:w-24 h-[95vh] w-16 justify-between items-end'>
+      <div className='fixed sm:top-6 top-4 right-6 z-10 text-[8px] sm:text-sm flex flex-col sm:w-24 h-[95vh] w-16 justify-between items-end'>
         <div className="">
           <Link href="#sectionProjets">
             <button className="w-full mb-2 sm:mb-4"> projets</button>
