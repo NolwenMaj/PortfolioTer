0b506ec1e0263475f79f5f40fb9431b69ed91f5f
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2023-04-09 14:04:51 +0200

    w on header dekstop

--- my-project/src/components/Header.js
+++ my-project/src/components/Header.js
@@ -6,7 +6,7 @@ import { mdiArrowUp } from '@mdi/js';
 
 export default function Header (){
     return(
-      <div className='space-x-4 font-display fixed sm:top-6 bottom-4 sm:right-6 z-10 text-sm flex-row flex sm:flex-col sm:h-[95vh] w-full justify-center sm:justify-between sm:items-end'>
+      <div className='space-x-4 font-display fixed sm:top-6 bottom-4 sm:right-6 z-10 text-sm flex-row flex sm:flex-col sm:h-[95vh] sm:w-24 w-full justify-center sm:justify-between sm:items-end'>
         <div className="flex flex-row space-x-4 sm:flex-col sm:w-24 sm:space-y-4 sm:space-x-0">
           <Link href="#sectionProjets">
             <button className="sm:w-full"> projets</button>
