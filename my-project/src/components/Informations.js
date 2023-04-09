623abcb1926d9f547a0b70bc3ed923ba141c6640
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2023-04-09 13:56:40 +0200

    pictos a propos

--- my-project/src/components/Informations.js
+++ my-project/src/components/Informations.js
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import Icon from '@mdi/react';
 import { mdiArrowTopRight } from '@mdi/js';
+import { mdiRayStartArrow } from '@mdi/js';
+import { mdiArrowURightBottom } from '@mdi/js';
+import { mdiGamepadSquare } from '@mdi/js';
 
 
 export default function Informations(){
@@ -11,6 +14,7 @@ export default function Informations(){
                         <div className="flex sm:flex-row flex-col gap-4">
                             <div className="flex sm:flex-row flex-col sm:items-center items-start gap-6">
                                     <h1 className="text-4xl text-[#F2511B] font-medium ">A PROPOS </h1>  
+                                    <div className='flex flex-row'>
                                     <div className="flex flex-row">
                                         <Image 
                                             src ="/pictoLinkedin.png"
@@ -32,22 +36,23 @@ export default function Informations(){
                                         <a target="_blank" href="https://github.com/NolwenMaj"><Icon className="hover:animate-bounce" path={mdiArrowTopRight} size={1} /></a>
                                     </div>
                                 </div>
+                            </div>
                         </div>
                         <div className="flex flex-col sm:grid sm:grid-cols-3 gap-12 font-thin ">
                         <div className="flex flex-col gap-4">
-                            <h2 className=" font-bold  text-xl">  🚦POINT DE DÉPART : LE DESIGN  </h2>
+                            <h2 className=" font-bold  text-xl"><span><Icon path={mdiRayStartArrow} size={2}/></span>POINT DE DÉPART : LE DESIGN  </h2> 
                             <p> J’ai étudié le Design d’espace, orienté Eco-Social, à l’Ecole Supérieure d’Art et de Design de Valenciennes. La pédagogie orientée projet de l’école m’a permis de développer de multiples compétences en <b>graphisme</b>, <b>méthodologies de recherche</b>, <b>prototypage</b>, et <b>gestion de projet</b>. 
                             </p>
                         </div>
                         <div className="flex flex-col gap-4">
-                            <h2 className=" font-bold  text-xl">  ↪️ VIRAGE : LE DÉVELOPPEMENT D’APPLICATIONS </h2>
+                            <h2 className=" font-bold  text-xl"><span><Icon path={mdiArrowURightBottom} size={2} /></span>VIRAGE : LE DÉVELOPPEMENT D’APPLICATIONS </h2>
                             <p>J’avais envie de comprendre le monde parallèle que constitue le numérique dans nos vie, alors j’ai pris la décision de me reconvertir. Pas n’importe où : à Ada Tech School, une école du numérique inclusive, avec une pédagogie orientée projet de groupes, un cadre bienveillant tout cela en format “long” de 9 mois de cours suivis de 12 mois d’alternance.
                             Je m’épanouis en Front mais je ne boude pas le Back ! 
                             <b> J’aime débugger, l’algorithmie, le clean code et la conception de base de donnée relationnelle.</b>
                             </p>
                         </div>
                         <div className="flex flex-col gap-4">
-                            <h2 className=" font-bold  text-xl" > 🧩 PAYSAGE : CONCEPTION LUDIQUE</h2>
+                            <h2 className=" font-bold  text-xl"><span><Icon path={mdiGamepadSquare} size={2} /></span>PAYSAGE : CONCEPTION LUDIQUE</h2>
                             <p> <b>J’aime les jeux : y jouer, en étudier, en critiquer, en conseiller, en illustrer et bien sûr en concevoir.</b> Ainsi, j’ai eu l’occasion développer un ensemble de compétences de la conception de la mécanique de jeu, au prototypage en passant par l'illustration lors de la conception et prototypage de plusieurs jeux :
                                 - le projet « Les Récréateurs » comprenant un ensemble de jeux pour faciliter la participation citoyenne en urbanisme, réalisés en binôme dans le cadre de mon Diplôme National Supérieur d'Expression Plastique.
                                 - une variété de prototypes de jeux sérieux, du jeu de carte à l'escape game lors de mon service civique auprès de l'association d'architectes de quartier ICI !
